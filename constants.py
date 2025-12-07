216bdd339d3d78f20fbf07f686d8652c2cc27e6b
nkuek <nkuek1@gmail.com>
2025-12-06 21:52:58 -0500

    add chrome path option

--- constants.py
+++ constants.py
@@ -4,11 +4,15 @@ import undetected_chromedriver as uc
 BASE_URL = "https://app.courtreserve.com"
 
 headless = os.environ.get("HEADLESS", "false").lower() == "true"
+chrome_path = os.environ.get("CHROME_PATH")  # Optional: path to Chrome binary
 
 options = uc.ChromeOptions()
 options.add_argument("--window-size=1920,1080")
 options.add_argument("--start-maximized")
 options.add_argument("--disable-extensions")
+
+if chrome_path:
+    options.binary_location = chrome_path
 
 if headless:
     options.add_argument("--disable-gpu")
